b7e07b20ba85e9f7dde20cdda7a08ee320cb0bdb
zerokinetic <mhemansakkthivel@gmail.com>
2025-07-09 11:03:03 +0530

    Implemented AsyncImage property

--- Pokedex/ContentView.swift
+++ Pokedex/ContentView.swift
@@ -13,7 +13,37 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             List(pokemon) { poke in
-                Text(poke.name)
+                HStack {
+                    VStack (alignment: .leading, spacing: 5) {
+                        Text(poke.name.capitalized)
+                            .font(.title)
+                        Text(poke.type.capitalized)
+                            .italic()
+                        Text(poke.description)
+                            .font(.caption)
+                            .lineLimit(2)
+                        
+                    }
+                    
+                    AsyncImage(url: URL(string: poke.imageURL)) { phase in
+                        switch phase {
+                        case .empty:
+                            ProgressView()
+                            
+                        case .success(let image):
+                            image.resizable()
+                                .interpolation(.none)
+                                .scaledToFit()
+                                .frame(width: 100, height: 100)
+                            
+                        case .failure:
+                            Image(systemName: "photo")
+                            
+                        @unknown default:
+                            EmptyView()
+                        }
+                    }
+                }
             }
             .navigationTitle("Pokemon")
         }
