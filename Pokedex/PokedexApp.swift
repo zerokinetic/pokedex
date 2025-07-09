b9c1b514e02ba1c451f5149790ce2b7e31c0618b
zerokinetic <mhemansakkthivel@gmail.com>
2025-07-09 10:09:39 +0530

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by heman on 09/07/25.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
